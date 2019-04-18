841a4ded8fabb6e9d69f9c46d1f8682fda5b0f2e
Sergey <mk-serg@yandex.ru>
2019-04-18 11:20:57 +0300

    HrBot refactoring
    Refactoring 3 defp funcs were added

--- internship_tasks/lib/hrbot.ex
+++ internship_tasks/lib/hrbot.ex
@@ -21,43 +21,18 @@ defmodule HrBot do
     """
 
     import HTTPoison
+    
     def hello(name, position \\ "сотрудник") do
         if is_bitstring(name) and is_bitstring(position) do
             try do  
-                # Генерируем форму для должности
-                url_pos = "https://ws3.morpher.ru/russian/declension?s=" <> String.replace(position, " " , "%20")
-                resp = HTTPoison.get! url_pos
-                body = resp.body
-                # <В> </В> В этом тэге заключена форма слова в винительном падеже
-                form_position = 
-                    Regex.run(~r/\<В\>[a-zA-Zа-яА-Я-\/0-9\s']+[ ]*<\/В\>/u, body)
-                    |> List.to_string() 
-                    # Избавляемся от тега <В> </В>
-                    |> String.slice(3..-1)
-                    |> String.slice(0..-5)
-
-                # Генерируем форму для имени
-                url_name = "https://ws3.morpher.ru/russian/declension?s=" <> String.replace(name, " " , "%20")
-                resp = HTTPoison.get! url_name
-                body = resp.body
-                # <В> </В> В этом тэге заключена форма слова в винительном падеже
-                form_name = 
-                    Regex.run(~r/\<В\>[a-zA-Zа-яА-Я-\/0-9\s]+[ ]*<\/В\>/u, body)
-                    |> List.to_string() 
-                    # Избавляемся от тега <В> </В>
-                    |> String.slice(3..-1)
-                    |> String.slice(0..-5)
+                # Определяем форму имени и должности
+                form_position = position_form(position)
+                form_name = name_form(name)
                 
                 # Если должность состоит из русских символов только. Так как сайт translate.ru другие не принимает 
                 if position == String.replace(position, ~r/[^а-яА-ЯщцушхъфырэчстьюЩЦУШЧЪФЫРЭЧСТЬЮ]/, "") do
-
-                    # Узнаем род должности (мужской или женский)
-                    url_pos_gender = "https://www.translate.ru/Grammar/ru-en/" <> String.replace(position, " " , "%20")
-                    resp = HTTPoison.get! url_pos_gender
-                    body = resp.body
-                    pos_gender = 
-                        Regex.run(~r/м.р.|ж.р./, body)
-                        |> List.to_string()
+                    # Определяем род должности (м/ж)
+                    pos_gender = position_gender(position)
 
                     # Проверяем, если гендер имени и род должности - женские, то заменяем
                     # слова "нашего нового" на "нашу новую".
@@ -82,4 +57,44 @@ defmodule HrBot do
         end         
     end
 
+    # Определяет форму должности
+    defp position_form(position) do
+        # Генерируем форму для должности
+        url_pos = "https://ws3.morpher.ru/russian/declension?s=" <> String.replace(position, " " , "%20")
+        resp = HTTPoison.get! url_pos
+        body = resp.body
+        # <В> </В> В этом тэге заключена форма слова в винительном падеже
+        form_position = 
+            Regex.run(~r/\<В\>[a-zA-Zа-яА-Я-\/0-9\s']+[ ]*<\/В\>/u, body)
+            |> List.to_string() 
+            # Избавляемся от тега <В> </В>
+            |> String.slice(3..-1)
+            |> String.slice(0..-5)
+    end
+
+    # Определяет форму имени
+    defp name_form(name) do
+        # Генерируем форму для имени
+        url_name = "https://ws3.morpher.ru/russian/declension?s=" <> String.replace(name, " " , "%20")
+        resp = HTTPoison.get! url_name
+        body = resp.body
+        # <В> </В> В этом тэге заключена форма слова в винительном падеже
+        form_name = 
+            Regex.run(~r/\<В\>[a-zA-Zа-яА-Я-\/0-9\s]+[ ]*<\/В\>/u, body)
+            |> List.to_string() 
+            # Избавляемся от тега <В> </В>
+            |> String.slice(3..-1)
+            |> String.slice(0..-5)
+    end
+
+    # Определяет род позиции (м/ж)
+    defp position_gender(position) do
+        # Узнаем род должности (мужской или женский)
+        url_pos_gender = "https://www.translate.ru/Grammar/ru-en/" <> String.replace(position, " " , "%20")
+        resp = HTTPoison.get! url_pos_gender
+        body = resp.body
+        pos_gender = 
+            Regex.run(~r/м.р.|ж.р./, body)
+            |> List.to_string()
+    end
 end
